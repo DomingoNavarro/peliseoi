f7fd7eb1a682b165d42fc3df8b6cfd77f612566a
DomingoNavarro <djno447@gmail.com>
2017-06-09 01:28:37 +0100

    Muchisimos cambios

--- js/controllers/home.controller.js
+++ js/controllers/home.controller.js
@@ -16,7 +16,11 @@
 		$scope.activo = false;
         $scope.selectedMovie = selectedMovie;
         $scope.getGenre = getGenre;
+		$scope.buscaPeli = buscaPeli;
         $scope.newMovie = {};
+		$scope.mejorValoradas = mejorValoradas;
+		$scope.popNow=popNow;
+		$scope.incoming=incoming;
 		activate();
 
 		////////////////
@@ -26,14 +30,15 @@
 		}
 		var baseUrl = "https://api.themoviedb.org/3/";
 /*RECOGEMOS TODAS LAS PELICULAS*/
+		//https://api.themoviedb.org/3/discover/movie?api_key=edf0f15a547c21b304bcfd7d8fefc700&language=es-ES&sort_by=revenue.asc&include_adult=true&include_video=false&page=1
 		function call() {
-			$http.get(baseUrl + "discover/movie?&primary_release_year=2017&api_key=edf0f15a547c21b304bcfd7d8fefc700&language=es-ES")
+			$http.get(baseUrl + "discover/movie?api_key=edf0f15a547c21b304bcfd7d8fefc700&language=es-ES&sort_by=revenue.asc&include_adult=true&include_video=false&page=1")
 
 				.then(function (response) {
-					console.log(response);
+
 					//$scope.data = response;
 					$scope.movies = $scope.movies.concat(response.data.results);
-					console.log($scope.movies);
+
 				})
 
 		}
@@ -44,9 +49,9 @@
 			$http
 				.get(baseUrl + "genre/movie/list?api_key=edf0f15a547c21b304bcfd7d8fefc700&language=es-ES")
 				.then(function (response) {
-					console.log(response);
+
 					$scope.genres = response.data.genres;
-					console.log($scope.genres);
+
 				})
 		}
         /*SELECCIONAMOS LA PELICULA PARA MODAL DESPUES*/
@@ -60,9 +65,42 @@
                 .get(baseUrl + "discover/movie?api_key=edf0f15a547c21b304bcfd7d8fefc700&language=es-ES&sort_by=popularity.desc&page=1&with_genres=" + genreId)
                 .then(function (response){
                      $scope.movies = response.data.results;
-                console.log(response.data);
+
 					  })
         }
+
+		function buscaPeli(peli){
+			//search/movie?api_key=edf0f15a547c21b304bcfd7d8fefc700&page=1&query=logan
+			 $http
+                .get(baseUrl + "search/movie?api_key=edf0f15a547c21b304bcfd7d8fefc700&page=1&query=" + peli)
+				.then(function (response){
+				 $scope.movies = response.data.results;
+			 })
+		}
+		function mejorValoradas(){
+			$http
+				.get(baseUrl + "discover/movie?api_key=edf0f15a547c21b304bcfd7d8fefc700&language=es-ES&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1")
+				.then(function (response){
+				$scope.movies = response.data.results;
+			})
+		}
+
+		function popNow(){
+			$http
+				.get(baseUrl + "discover/movie?api_key=edf0f15a547c21b304bcfd7d8fefc700&language=es-ES&sort_by=popularity.desc&include_adult=true&include_video=false&page=1")
+				.then(function (response){
+				$scope.movies = response.data.results;
+			})
+		}
+
+		function incoming(){
+			$http
+				.get(baseUrl + "discover/movie?api_key=edf0f15a547c21b304bcfd7d8fefc700&language=es-ES&sort_by=primary_release_date.desc&include_adult=false&include_video=false&page=1")
+				.then(function (response){
+				$scope.movies = response.data.results;
+			})
+		}
+		buscaPeli();
 		call();
 		genres();
 
